731db7989c21f4a805160bba051334e163dde307
batuncer <tuncerbaki@gmail.com>
2024-04-19 19:06:17 +0100

    misspell is corrected

--- src/layout/Filter.jsx
+++ src/layout/Filter.jsx
@@ -13,20 +13,20 @@ export function Filter({ setPrice, setRate, setCategory }) {
         <div className="w-[200px] mt-3 p-1">
             <div className="text-2xl">Filter</div>
             <div className="flex items-center gap-2 my-2">
-                <input onChange={e => setPrice(prev => ({ ...prev, min: e.target.value }))} className="border w-16 p-1 outline-none" type="number" placeholder="Min" />
-                <input onChange={e => setPrice(prev => ({ ...prev, max: e.target.value }))} className="border w-16 p-1 outline-none" type="number" placeholder="Max" />
+                <input onChange={e => setPrice(prev => ({ ...prev, min: e.target.value }))} className="border w-16 p-1 outline-none text-sm cursor-pointer" type="number" placeholder="Min" />
+                <input onChange={e => setPrice(prev => ({ ...prev, max: e.target.value }))} className="border w-16 p-1 outline-none text-sm cursor-pointer" type="number" placeholder="Max" />
             </div>
             <div className="my-2 text-xl">Category</div>
             {
                 categoryList.map((category, i) => (
-                    <div className="text-sm" onClick={() => setCategory(category)} key={i}>{category}</div>
+                    <div className="text-sm text-sm cursor-pointer" onClick={() => setCategory(category)} key={i}>{category}</div>
                 ))
             }
             <hr />
             <div className="my-2 text-xl">Rating</div>
             {
                 ratingList.map((rating, i) => (
-                    <div onClick={() => setRate(rating)} className="text-sm" key={i}>{rating}</div>
+                    <div onClick={() => setRate(rating)} className="text-sm cursor-pointer" key={i}>{rating}</div>
                 ))
             }
         </div>
