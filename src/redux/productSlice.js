13ca489c09f2ff5661b8b77cb959d857bab63831
batuncer <tuncerbaki@gmail.com>
2024-04-19 19:05:24 +0100

    Products and search products are seperated on redux

--- src/redux/productSlice.js
+++ src/redux/productSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { config } from "../config";
 
 const initialState = {
   products: [],
@@ -6,27 +7,35 @@ const initialState = {
   loading: false,
 };
 
-export const getProducts = createAsyncThunk("products", async (params) => {
-  let link = `http://localhost:8080/products?keyword=${
-    params.keyword
-  }&rating[gte]=${params.rate || 0}&price[gte]=${
-    params.price.min || 0
-  }&price[lte]=${params.price.max || 30000}`;
-
-  if (params.catagory) {
-    link = `http://localhost:8080/products?keyword=${
-      params.keyword
-    }&rating[gte]=${params.rate || 0}&price[gte]=${
-      params.price.min || 0
-    }&price[lte]=${params.price.max || 30000}&catagory=${params.catagory}`;
-  }
-
-  const response = await fetch(link);
+export const getProducts = createAsyncThunk("products", async () => {
+  const response = await fetch(`${config.api.url}/products`);
   return await response.json();
 });
 
+export const getProductsSearch = createAsyncThunk(
+  "products2",
+  async (params) => {
+    let link = `${config.api.url}/products?keyword=${
+      params.keyword
+    }&rating[gte]=${params.rate || 0}&price[gte]=${
+      params.price.min || 0
+    }&price[lte]=${params.price.max || 30000}`;
+
+    if (params.category) {
+      link = `${config.api.url}/products?keyword=${
+        params.keyword
+      }&rating[gte]=${params.rate || 0}&price[gte]=${
+        params.price.min || 0
+      }&price[lte]=${params.price.max || 30000}&catagory=${params.category}`;
+    }
+
+    const response = await fetch(link);
+    return await response.json();
+  }
+);
+
 export const getProductDetails = createAsyncThunk("product", async (id) => {
-  const response = await fetch(`http://localhost:8080/products/${id}`);
+  const response = await fetch(`${config.api.url}/products/${id}`);
   return await response.json();
 });
 
@@ -49,6 +58,13 @@ export const productSlice = createSlice({
       .addCase(getProductDetails.fulfilled, (state, action) => {
         state.loading = false;
         state.product = action.payload;
+      })
+      .addCase(getProductsSearch.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getProductsSearch.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
       });
   },
 });
