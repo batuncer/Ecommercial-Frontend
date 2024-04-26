e0bf52dfa1c32b8eafbc90c4aee754e0bf54829e
batuncer <tuncerbaki@gmail.com>
2024-04-26 17:56:18 +0100

    singup page is created with tailwind and react hook

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,21 +1,11 @@
 import React from "react";
 import SignUpForm from "../components/auth-components/SingUpForm";
-import { useAuthContext } from "../auth/useAuthContext";
 
 export default function SignUp() {
-    const { login, register, token } = useAuthContext();
-
-    const handleSignup = () => {
-        register(token);
-    }
-
-    const handleLogin = (token) => {
-        login(token);
-    }
 
     return (
         <div>
-            <SignUpForm handleSignup={handleSignup} handleLogin={handleLogin} />
+            <SignUpForm />
         </div>
     );
 }
