f6f2ceb9dedb44fd4314067334033c97acf1af64
batuncer <tuncerbaki@gmail.com>
2024-05-03 17:49:00 +0100

    input component is created to use for product adding

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
-const Input = ({ placeHolder, value, name, id, type }) => {
+const Input = ({ placeholder, value, name, id, type, onChange }) => {
     return (
-        <div>Input</div>
+        <input
+            className="w-full h-10 p-2 outline-none rounded-md border border-gray-300"
+            id={id}
+            type={type}
+            name={name}
+            value={value}
+            placeholder={placeholder}
+            onChange={onChange}
+        />
     )
 }
 
